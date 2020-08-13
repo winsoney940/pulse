3eef3295bf3e2c85ea011976219c3c29b7829d6e
Georgiy <kositskiy2003@mail.ru>
2020-08-13 15:54:15 +0300

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,168 @@
+$(document).ready(function () {
+  //     $('.carousel__inner').slick({
+  //         speed: 1200,
+  //         prevArrow: '<button type="button" class="slick-prev"><img src="/icons/left_arrow.svg"></button>',
+  //         nextArrow: '<button type="button" class="slick-next"><img src="/icons/right_arrow.svg"></button>',
+  //         responsive: [
+  //             {
+  //                 breakpoint: 992,
+  //                 settings: {
+  //                   dots: true,
+  //                   arrows: false
+  //                 }
+  //             }
+  //         ]
+  //       });
+  // });     
+
+  const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    controls: false,
+    nav: false
+  });
+
+  document.querySelector('.prev').addEventListener('click', function () {
+    slider.goTo('prev');
+  });
+
+  document.querySelector('.next').addEventListener('click', function () {
+    slider.goTo('next');
+  });
+
+  (function ($) {
+    $(function () {
+
+      $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this)
+            .index()).addClass('catalog__content_active');
+      });
+
+      function toggleSlide(item) {
+        $(item).each(function (i) {
+          $(this).on('click', function (e) {
+            e.preventDefault();
+            $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+            $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+          })
+        })
+      };
+
+      toggleSlide('.catalog-item__link');
+      toggleSlide('.catalog-item__back');
+
+      // Modal
+
+      $('[data-modal=consultation]').on('click', function () {
+        $('.overlay, #consultation').fadeIn('slow');
+      });
+      $('.modal__close').on('click', function () {
+        $('.overlay, #consultation, #thanks, #order').fadeOut('slow')
+      });
+      $('.button_mini').each(function (i) {
+        $(this).on('click', function () {
+          $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+          $('.overlay, #order').fadeIn('slow')
+        });
+      });
+
+      // $('#consultation-form').validate();
+      // $('#consultation form').validate( {
+      //   rules: {
+      //     name: {
+      //       required: true,
+      //       minlength: 2
+      //     },
+      //     phone: "required",
+      //     email: {
+      //       required: true,
+      //       email: true
+      //     }
+      //   },
+      //   messages: {
+      //     name: {
+      //       minlength: jQuery.validator.format("Введите {0} символа!"),
+      //       required: "Пожалуйста, введите своё имя"
+      //      },
+      //     phone: "Пожалуйста, введите свой номер телефона",
+      //     email: {
+      //       required: "Пожалуйста, введите свою почту",
+      //       email: "Неправильно введён адрес почты"
+      //     }
+      //   }
+      // });
+      // $('#order form').validate();
+
+      function validateForms(form) {
+        $(form).validate({
+          rules: {
+            name: {
+              required: true,
+              minlength: 2
+            },
+            phone: "required",
+            email: {
+              required: true,
+              email: true
+            }
+          },
+          messages: {
+            name: {
+              minlength: jQuery.validator.format("Введите {0} символа!"),
+              required: "Пожалуйста, введите своё имя"
+            },
+            phone: "Пожалуйста, введите свой номер телефона",
+            email: {
+              required: "Пожалуйста, введите свою почту",
+              email: "Неправильно введён адрес почты"
+            }
+          }
+        });
+      };
+
+      validateForms('#consultation-form');
+      validateForms('#consultation form');
+      validateForms('#order form');
+
+      $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+      $('form').submit(function (e) {
+        e.preventDefault();
+        $.ajax({
+          type: "POST",
+          url: "mailer/smart.php",
+          data: $(this).serialize()
+        }).done(function () {
+          $(this).find("input").val("");
+          $('#consultation, #order').fadeOut();
+          $('.overlay, #thanks').fadeIn('slow');
+
+          $('form').trigger('reset');
+        });
+        return false;
+      });
+
+      // Smooth scroll and pageup
+
+      $(window).scroll(function () {
+        if ($(this).scrollTop() > 1600) {
+          $('.pageup').fadeIn();
+        } else {
+          $('.pageup').fadeOut();
+        }
+      });
+
+      $("a[href=#up]").click(function () {
+        const _href = $(this).attr("href");
+        $("html, body").animate({
+          scrollTop: $(_href).offset().top + "px"
+        });
+        return false;
+      });
+    });
+  })(jQuery);
+});
